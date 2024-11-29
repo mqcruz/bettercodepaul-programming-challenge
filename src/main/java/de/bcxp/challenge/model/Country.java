0ed6cbfa123ba0992615fa699353ab5a3c57ea8b
Maria <maria.quintela.cruz@gmail.com>
2024-11-29 14:34:37 +0000

    Add necessary fields to Country and Weather

--- src/main/java/de/bcxp/challenge/model/Country.java
+++ src/main/java/de/bcxp/challenge/model/Country.java
@@ -1,4 +1,13 @@
 package de.bcxp.challenge.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class Country {
+    String name;
+    long population;
+    double area;
 }
--- src/main/java/de/bcxp/challenge/model/Weather.java
+++ src/main/java/de/bcxp/challenge/model/Weather.java
@@ -1,4 +1,13 @@
 package de.bcxp.challenge.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class Weather {
+    int day;
+    double maxTemp;
+    double minTemp;
 }
