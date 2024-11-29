3079e4d48a9b5c0d693c52a695d812a523479f57
Maria <maria.quintela.cruz@gmail.com>
2024-11-29 18:20:42 +0000

    Add CsvFileReaderService to read data from the data files and return a list with the fields and the values

--- src/main/java/de/bcxp/challenge/service/CsvFileReaderService.java
+++ src/main/java/de/bcxp/challenge/service/CsvFileReaderService.java
@@ -1,5 +1,40 @@
 package de.bcxp.challenge.service;
 
-public class CsvFileReaderService implements FileReaderService {
+import de.bcxp.challenge.mapper.Mapper;
+import de.bcxp.challenge.mapper.MapperRegistry;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvFileReaderService<T> implements FileReaderService<T> {
+
+    @SuppressWarnings("unchecked")
+    public List<T> readData(String filePath, String delimiter) {
+        String fileName = filePath.substring(filePath.lastIndexOf("/") +1);
+        Mapper<T> mapper = (Mapper<T>) MapperRegistry.getMapper(fileName);
+
+        List<T> results = new ArrayList<>();
+        try(BufferedReader fileReader = new BufferedReader(new FileReader(filePath))) {
+            String headerLine = fileReader.readLine();
+            if(headerLine == null) {
+                throw new RuntimeException("Header line is empty");
+            }
+
+            String[] headers = headerLine.split(delimiter);
+            String line;
+
+            while ((line = fileReader.readLine()) != null) {
+                String[] values = line.split(delimiter);
+                results.add(mapper.map(values, headers));
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return results;
+    }
 
 }
