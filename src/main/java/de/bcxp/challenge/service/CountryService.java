7f31f366165ce15aa106681f6b5a53eb8e521993
Maria <maria.quintela.cruz@gmail.com>
2024-11-29 18:18:36 +0000

    Add CountryService class to handle country data processing and return country with the highest population density

--- src/main/java/de/bcxp/challenge/service/CountryService.java
+++ src/main/java/de/bcxp/challenge/service/CountryService.java
@@ -1,4 +1,21 @@
 package de.bcxp.challenge.service;
 
+import de.bcxp.challenge.model.Country;
+
+import java.util.Comparator;
+import java.util.List;
+
 public class CountryService {
+
+    private final List<Country> countryList;
+
+    public CountryService(CsvFileReaderService csvFileReaderService) {
+        String filePath = "src/main/resources/de/bcxp/challenge/countries.csv";
+        String delimiter = ";";
+        countryList = csvFileReaderService.readData(filePath, delimiter);
+
+    }
+    public String getHighestPopulationDensityCountry() {
+        return this.countryList.stream().max(Comparator.comparingDouble(Country::getPopulationDensity)).orElseThrow().getName();
+    }
 }
